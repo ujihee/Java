008f36f3672b9c861e058e38bd8cdbc89d2eaae7
tmfflavndn <tmfflavndn@gmail.com>
2025-07-15 12:18:13 +0900

    add ch05

--- ch05/src/sub5/MethodOverridingTest.java
+++ ch05/src/sub5/MethodOverridingTest.java
@@ -18,22 +18,30 @@ class AAA{
 	}
 }
 class BBB extends AAA{
+	
+	@Override
 	public void method2() {
+		//부모클래스 method2를 Override하기 때문에 어노테이션 @Override 표시
 		System.out.println("BBB:method2...");
 	}
-	public void method3(int a) {
+	
+	public void method3(int a) { // Overload 메서드
 		System.out.println("BBB:method3...");
 	}
 }
 class CCC extends BBB {
-
+	
+	@Override
 	public void method1() {
 	System.out.println("CCC:method1...");
 	}
+	
+	@Override
 	public void method2() {
 	System.out.println("CCC:method2...");
 	}
-	public void method3(int a, int b) {
+	
+	public void method3(int a, int b) { //Overload
 	System.out.println("CCC:method3...");
 	}
 }
@@ -42,10 +50,16 @@ public class MethodOverridingTest {
 	public static void main(String[] args) {
 		CCC c = new CCC();
 		
+		//오버라이드 메서드 실습
 		c.method1();
 		c.method2();
 		c.method3();
 		c.method3(1);
 		c.method3(1, 2);
+		
+		//Sedan 클래스 오버라이드 메서드 실습
+		Sedan sonata = new Sedan("소나타", "흰색", 0, 2000);
+		sonata.speedUp(200);
+		sonata.show();
 	}
 }
