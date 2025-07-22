0af7e5ca34112bea4d56e8103f629b85db4d9882
tmfflavndn <tmfflavndn@gmail.com>
2025-07-22 11:52:08 +0900

    add ch08

---
+++ ch08/src/sub1/FruitBox.java
@@ -0,0 +1,15 @@
+package sub1;
+
+public class FruitBox<T> {// 제네릭 클래스 FruitBox의 치환변수 T를 선언
+	
+	private T fruit; //치환변수T
+	
+	public T getFruit() {
+		return fruit;
+	}
+	
+	public void setFruit(T fruit) {
+		this.fruit = fruit;
+	}
+
+}
